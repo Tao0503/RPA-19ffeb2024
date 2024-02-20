47b95bea4b56d3fae7740acd05edb8a5f46d0251
Tao0503 <160452322+Tao0503@users.noreply.github.com>
2024-02-20 15:32:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-  return(render_template("index.html"))
+    return(render_template("index.html"))
+
+@app.route("/main",methods=["GET","POST"])
+def index():
+    r = request.form.get("r")
+    return(render_template("main.html",r=r))
 
 @app.route("/image_gpt",methods=["GET","POST"])
-def main():
-  r = request.form.get("r")
+def image_gpt():
   return(render_template("image_gpt.html"))
 
 if __name__ == "__main__":
