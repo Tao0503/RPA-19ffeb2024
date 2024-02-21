7eeb2769ff2685375c82934d9766f0d35abd45d5
Tao0503 <160452322+Tao0503@users.noreply.github.com>
2024-02-21 10:19:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import replicate
 import os
 
 app = Flask(__name__)
-
+os.environ["REPLICATE_API_TOKEN"]="r8_STBS634px9hv4QKE4KsosC44Jk5Roqn2MRG5v"
 r = ""
 first_time = 1
 
@@ -23,12 +23,17 @@ def image_gpt():
     return(render_template(i="image_gpt.html"))
 @app.route("/image_result",methods=["GET","POST"])
 def image_result():
-
-
+    q = request.form.fet("q")
+    r = replicate.run(
+    "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
+    input={
+        "prompt": q,
+        }
+    )
+    time.sleep(10)
+    return(render_template(i="image_result.html",r=r[0]))
     
-    return(render_template(i="image_gpt.html"))
 @app.route("/end",methods=["GET","POST"])
-
 def end():
   return(render_template("image_gpt.html"))
 
