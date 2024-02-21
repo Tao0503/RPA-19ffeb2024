1223b442942d510169570c24ad8030abfd016b88
Tao0503 <160452322+Tao0503@users.noreply.github.com>
2024-02-21 10:36:50 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask,request,render_template
 import replicate
 import os
+import time
 
 app = Flask(__name__)
-os.environ["REPLICATE_API_TOKEN"]="r8_STBS634px9hv4QKE4KsosC44Jk5Roqn2MRG5v"
+os.environ["REPLICATE_API_TOKEN"]="r8_2idkAutIh1jCAVVRIbEDgqt9zNUdbhG2cS1AF"
+
 r = ""
 first_time = 1
 
@@ -18,12 +20,14 @@ def main():
         r = request.form.get("r")
         first_time=0
     return(render_template("main.html",r=r))
+
 @app.route("/image_gpt",methods=["GET","POST"])
 def image_gpt():
-    return(render_template(i="image_gpt.html"))
+    return(render_template("image_gpt.html"))
+
 @app.route("/image_result",methods=["GET","POST"])
 def image_result():
-    q = request.form.fet("q")
+    q = request.form.get("q")
     r = replicate.run(
     "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf",
     input={
@@ -31,13 +35,14 @@ def image_result():
         }
     )
     time.sleep(10)
-    return(render_template(i="image_result.html",r=r[0]))
-    
+    return(render_template("image_result.html",r=r[0]))
+
 @app.route("/end",methods=["GET","POST"])
 def end():
-  return(render_template("image_gpt.html"))
+    global first_time
+    first_time = 1
+    return(render_template("end.html"))
 
 if __name__ == "__main__":
     app.run()
-
 
