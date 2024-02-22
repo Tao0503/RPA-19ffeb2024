44d52ff8fd7b2d2d7bb309c9cad74024de84fb99
Tao0503 <160452322+Tao0503@users.noreply.github.com>
2024-02-22 11:38:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-app.py
-
 from flask import Flask,request,render_template
 import replicate
 import os
 import time
+from openai import OpenAI
 
-os.environ["REPLICATE_API_TOKEN"]="r8_2idkAutIh1jCAVVRIbEDgqt9zNUdbhG2cS1AF"
 
+openai_api_key=os.getenv("OPENAI_API_TOKEN")
+os.environ["REPLICATE_API_TOKEN"]="r8_Uh1JCGaGbackyghfdhi3ZFC6Hz1t6Ml2trhLC"
+model = OpenAI(api_key="sess-RfilxHvcxVZZLy9ALBr7mDJnrhXh0V8W4HjTDHA8")
 app = Flask(__name__)
 
 r = ""
@@ -31,7 +32,19 @@ def text_gpt():
 @app.route("/text_result",methods=["GET","POST"])
 def text_result():
     q = request.form.get("q")
-    return(render_template("text_result.html",r="API not ready"))
+    r = model.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {
+            "role":"user",
+            "content":q
+        }
+    ],
+)
+    
+    time.sleep(5)
+    return(render_template("text_result.html",r=r.choices[0].message.content))
+
 
 @app.route("/image_gpt",methods=["GET","POST"])
 def image_gpt():
@@ -49,11 +62,13 @@ def image_result():
     time.sleep(10)
     return(render_template("image_result.html",r=r[0]))
 
+
 @app.route("/end",methods=["GET","POST"])
 def end():
-    global first_time,r
+    global first_time
     first_time = 1
-    return(render_template("end.html",r=r))
+    return(render_template("end.html"))
+
 
 if __name__ == "__main__":
     app.run()
