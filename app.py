0fa33952acd0f7077801c123ed2f53544edc888c
Tao0503 <160452322+Tao0503@users.noreply.github.com>
2024-03-01 21:15:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,19 +7,19 @@ app = Flask(__name__)
 
 first_time = 1
 
-@app.route("/main",methods=["GET","POST"])
+@app.route("/main/",methods=["GET","POST"])
 def main():
     global first_time
     if first_time==1:
         first_time=0
     return(render_template("main.html"))
     
-@app.route("/about_mrs_huang", methods=["GET", "POST"])
+@app.route("/about_mrs_huang/", methods=["GET", "POST"])
 def about_mrs_huang():
     return render_template("about_mrs_huang.html")
 
 
-@app.route("/end",methods=["GET","POST"])
+@app.route("/end/",methods=["GET","POST"])
 def end():
     global first_time
     first_time = 1
