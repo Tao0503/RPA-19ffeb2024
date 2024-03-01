63d99018f6c0e084123bfac2bf3ee9fddf3736cf
Tao0503 <160452322+Tao0503@users.noreply.github.com>
2024-03-01 18:46:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,27 +2,21 @@ from flask import Flask,request,render_template
 import replicate
 import os
 import time
-from openai import OpenAI
 
 app = Flask(__name__)
 
-r = ""
 first_time = 1
 
 @app.route("/main",methods=["GET","POST"])
 def main():
-    global r,first_time
+    global first_time
     if first_time==1:
-        r = request.form.get("r")
         first_time=0
-    return(render_template("main.html",r=r))
+    return(render_template("main.html")
     
 @app.route("/about_mrs_huang", methods=["GET", "POST"])
 def about_mrs_huang():
     return render_template("about_mrs_huang.html")
-
-if __name__ == "__main__":
-    app.run(debug=True)
 
 
 @app.route("/end",methods=["GET","POST"])
